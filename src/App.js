51d6a6bd4004a9c1b4f70344125e4b716c336e8e
Pi Rho <13998+pyrotechnick@users.noreply.github.com>
2021-09-24 09:22:40 +1000

    build

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 function App({ pdf, svg }) {
   return (
     <>
-      {pdf ? <img src="/pdf.svg" alt="" /> : null}
+      {pdf ? <img src="./pdf.svg" alt="" /> : null}
       {svg ? (
         <svg viewBox="0 0 512 512" style={{ width: 512, height: 512 }}>
           <text x="50%" y="50%" style={{ fontSize: "72px" }}>
