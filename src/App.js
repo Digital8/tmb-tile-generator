77b0d4083aaab71081a9afcb6a5609bfb596f756
Pi Rho <13998+pyrotechnick@users.noreply.github.com>
2021-09-24 09:20:19 +1000

    build

--- src/App.js
+++ src/App.js
@@ -1,38 +1,37 @@
 import PropTypes from "prop-types";
 import "./App.css";
-import Shapes from "./components/shapes/shapes";
-import Logos from "./components/logos/logos";
-import { ReactComponent as MakeLogo } from "./logo.svg";
-import { ReactComponent as ReactLogo } from "./react-logo.svg";
 
-function App({ headline, showLogos, backgroundImage }) {
+function App({ pdf, svg }) {
   return (
-    <svg viewBox="0 0 512 512" style={{ width: 512, height: 512 }}>
-      <text x="50%" y="50%" style={{ fontSize: "72px" }}>
-        Test
-      </text>
-      <rect
-        x="0"
-        y="0"
-        width="512"
-        height="512"
-        fill="none"
-        stroke="hotpink"
-      ></rect>
-    </svg>
+    <>
+      {pdf ? <img src="/pdf.svg" alt="" /> : null}
+      {svg ? (
+        <svg viewBox="0 0 512 512" style={{ width: 512, height: 512 }}>
+          <text x="50%" y="50%" style={{ fontSize: "72px" }}>
+            Test
+          </text>
+          <rect
+            x="0"
+            y="0"
+            width="512"
+            height="512"
+            fill="none"
+            stroke="hotpink"
+          ></rect>
+        </svg>
+      ) : null}
+    </>
   );
 }
 
 App.propTypes = {
-  headline: PropTypes.string,
-  showLogos: PropTypes.string,
-  backgroundImage: PropTypes.string,
+  pdf: PropTypes.bool,
+  svg: PropTypes.bool,
 };
 
 App.defaultProps = {
-  headline: "Hello World",
-  showLogos: true,
-  backgroundImage: "",
+  pdf: true,
+  svg: false,
 };
 
 export default App;
